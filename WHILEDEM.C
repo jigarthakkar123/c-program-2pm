2e10c7b05e40d4112e76ea19cd891cc43ae0d2c6
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2025-05-02 14:40:21 +0530

    First

---
+++ WHILEDEM.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	clrscr();
+	printf("\nEnter N : ");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		printf("\nTops Technologies");
+		n=n-1;
+	}
+	getch();
+}
